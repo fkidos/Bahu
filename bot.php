71a3a53e3069e7d9278f89472d5ad508566e40e4
LoghKhezri <125271176+fkidos@users.noreply.github.com>
2023-09-15 11:07:03 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,7 +8,7 @@ function bot($method,$datas=[]){
 	$url = "https://api.telegram.org/bot".API_KEY."/".$method;
 	$ch = curl_init();
     curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true)
     curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
     $res = curl_exec($ch);
     if(curl_error($ch)){
