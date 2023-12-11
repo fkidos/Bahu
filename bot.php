1c6b6fa9fb5fd43ce00f76bf5404d9d1c665459a
LoghKhezri <125271176+fkidos@users.noreply.github.com>
2023-12-11 12:03:12 +0100

    Update bot.php

--- bot.php
+++ bot.php
@@ -76,7 +76,6 @@ $servername = "localhost";
 $username = "your_username";
 $password = "your_password";
 $dbname = "your_database";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
